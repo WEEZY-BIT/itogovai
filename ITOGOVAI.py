43ab03e27c0548dbae86941c7ab8980226ffc781
Radik <radik.wood@yandex.ru>
2023-12-24 13:51:46 +0300

    dopolnenie v shapke

--- ITOGOVAI.py
+++ ITOGOVAI.py
@@ -1,11 +1,1 @@
-n = int(input("Введите количество строк в массиве: "))
-arr = []
-for i in range(n):
-    arr.append(input("Введите строку: "))
 
-new_arr = []
-for string in arr:
-    if len(string) <= 3:
-        new_arr.append(string)
-
-print(new_arr)
